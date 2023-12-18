605189f25e129b0a5295ba57e5b619e3f434bfce
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2023-12-18 15:14:07 +0100

    fertig -> :)

--- Link_ExtensionMethods/MyExtensionMethods.cs
+++ Link_ExtensionMethods/MyExtensionMethods.cs
@@ -1,9 +1,179 @@
 ﻿
 
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 namespace Link_ExtensionMethods
 {
     public static class MyExtensionMethods
     {
+        public static List<T> Take<T>(this List<T> list, int anzahl)
+        {
+            List<T> list2 = new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i <= anzahl)
+                {
+                    list2.Add(list[i]);
+                }
+            }
+            return list2;
+        }
+
+        public static List<T> Skip<T>(this List<T> list, int anzahl)
+        {
+            List<T> list2 = new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i > anzahl)
+                {
+                    list2.Add(list[i]);
+                }
+            }
+            return list2;
+        }
+
+        public static List<T> Distinct<T>(this List<T> list)
+        {
+            List<T> list2 = new List<T>();
+            foreach (var item in list)
+            {
+                if (!list2.Contains(item))
+                {
+                    list2.Add(item);
+                }
+            }
+            return list2;
+        }
+
+        public static List<T> MyReverse<T>(this List<T> list)
+        {
+            List<T> returnList = new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                returnList.Insert(0, list[i]);
+            }
+            return returnList;
+        }
+
+        public static T MyFirst<T>(this List<T> list)
+        {
+            if (list != null)
+            {
+                return list[0];
+            }
+            else
+            {
+                throw new Exception("Kein Element");
+            }
+        }
+
+        public static T MyElementAt<T>(this List<T> list, int index)
+        {
+            return list[index];
+        }
+
+        public static T MyFirstOrDefault<T>(this List<T> list)
+        {
+            if (list != null)
+            {
+                return MyFirst(list);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public static List<T> MyTakeWhile<T>(this List<T> list, Predicate<T> p)
+        {
+            List<T> list2 = new List<T>();
+            int count = 0;
+            while (count < list.Count && p(list[count]))
+            {
+                list2.Add(list[count]);
+                count++;
+            }
+            return list2;
+        }
+
+        public static List<T> MySkipWhile<T>(this List<T> list, Predicate<T> p)
+        {
+            List<T> list2 = new List<T>();
+            int index = 0;
+            while (index < list.Count && p(list[index]))
+            {
+                index++;
+            }
+
+            while (index < list.Count)
+            {
+                list2.Add(list[index]);
+                index++;
+            }
+            
+            
+            return list2;
+        }
+
+        public static List<TRes> MySelect<T,TRes>(this List<T> list,Func<T,TRes> f)
+        {
+            List<TRes> list2 = new List<TRes>();
+            foreach(T item in list)
+            {
+                list2.Add(f(item));
+            }
+            return list2;
+        }
+
+        public static List<T> MyOrderBy<T>(this List<T> list,Comparison<T> comparison)
+        {
+            List<T> returnList = new List<T>();
+            foreach(T item in list)
+            {
+                int i = 0;
+                while (i < list.Count && i < returnList.Count && comparison(item, returnList[i]) > 0)
+                {
+                    i++;
+                }
+
+                returnList.Insert(i, item);
+            }
+            return returnList;
+        }
+        public static List<T> MyOrderBydown<T>(this List<T> list, Comparison<T> comparison)
+        {
+            List<T> returnList = new List<T>();
+            foreach (T item in list)
+            {
+                int i = 0;
+                while (i < list.Count && i < returnList.Count && comparison(item, returnList[i]) < 0)
+                {
+                    i++;
+                }
+
+                returnList.Insert(i, item);
+            }
+            return returnList;
+        }
+
+        public static List<T> MyWhere<T>(this List<T> list, Predicate<T> p)
+        {
+            List<T> list2 = new List<T>();
+            foreach(var item in list)
+            {
+                if(p(item))
+                {
+                    list2.Add(item);
+                }
+            }
+
+            return list2;
+        }
+
+
+
         public static int Count<T>(this List<T> list)
         {
             int counter = 0;
@@ -34,9 +204,9 @@ namespace Link_ExtensionMethods
         public static int Max(this List<int> list)
         {
             int max = list[0];
-            foreach(var item in list)
+            foreach (var item in list)
             {
-                if(item > max)
+                if (item > max)
                 {
                     max = item;
                 }
@@ -78,28 +248,29 @@ namespace Link_ExtensionMethods
             return (double)sumround;
         }
 
-        public static double Average(this List<double> list)
-        {
-            double sum = Sum(list);
-            double count = Count(list);
-            decimal average = (decimal)(sum / count);
-            average = decimal.Round(average, 2);
-            return (double)average;
-        }
+        //Des hätte ma spoan kinna
+        //public static double Average(this List<double> list)
+        //{
+        //    double sum = Sum(list);
+        //    double count = Count(list);
+        //    decimal average = (decimal)(sum / count);
+        //    average = decimal.Round(average, 2);
+        //    return (double)average;
+        //}
 
-        public static double Max(this List<double> list)
-        {
-            double max = list[0];
-            foreach (var item in list)
-            {
-                if (item > max)
-                {
-                    max = item;
-                }
-            }
-            decimal max2 = decimal.Round((decimal)max,2);
-            return (double)max2;
-        }
+        //public static double Max(this List<double> list)
+        //{
+        //    double max = list[0];
+        //    foreach (var item in list)
+        //    {
+        //        if (item > max)
+        //        {
+        //            max = item;
+        //        }
+        //    }
+        //    decimal max2 = decimal.Round((decimal)max,2);
+        //    return (double)max2;
+        // }
 
         public static List<T> Where<T>(this List<T> inputList, Predicate<T> predicate)
         {
--- Link_ExtensionMethods/Program.cs
+++ Link_ExtensionMethods/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Link_ExtensionMethods;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 internal class Program
 {
@@ -16,7 +18,9 @@ internal class Program
         integers = new List<int> { 5, 76, 3, 93, 143, 5, 11, 67, 5 };
         doubles = new List<double> { 1.23, 68.25, 44.55, 96.12, 393.4567, 2.45 };
         strings = new List<string> { "Hansi", "Pauli", "Heinzi", "Susi", "Pepi" };
-
+        WriteList(integers);
+        WriteList(doubles);
+        WriteList(strings);
 
         //Befüllen der Listen mit folgenden Daten:
         patients = new List<Patient>();
@@ -49,6 +53,56 @@ internal class Program
         foreach (Patient patient in PatientenGR50)
         {
             Console.WriteLine(patient);
+        }
+        Console.WriteLine("Reverse List");
+        List<int> reverseList = integers.MyReverse();
+        foreach (int number in reverseList)
+        {
+            Console.WriteLine(number);
+        }
+        List<int> ints = new List<int>();
+        List<string> strings2 = new List<string>();
+        ints = integers.Take(3);
+        Console.WriteLine("Take");
+        WriteList(ints);
+        Console.WriteLine("Skip");
+        ints = integers.Skip(3);
+        WriteList(ints);
+        Console.WriteLine("Distinct");
+        ints = integers.Distinct();
+        WriteList(ints);
+        Console.WriteLine("Reverse");
+        strings2 = strings.MyReverse();
+        WriteList(strings2);
+        Console.WriteLine("Ein Element");
+        Console.WriteLine(strings.MyFirst());
+        Console.WriteLine("Ein Element an stelle n");
+        Console.WriteLine(strings.MyElementAt(2));
+        Console.WriteLine("Take While");
+        ints = integers.MyTakeWhile(x=> x < 143);
+        WriteList(ints);
+        Console.WriteLine("Skip While");
+        ints = integers.MySkipWhile(x => x < 143);
+        WriteList(ints);
+        Console.WriteLine("Where");
+        ints = integers.MyWhere(x => x == 5);
+        WriteList(ints);
+        Console.WriteLine("Select");
+        strings2 = integers.MySelect(x => x.ToString() + "blbllb");
+        WriteList(strings2);
+        Console.WriteLine("Order by auf");
+        ints = integers.MyOrderBy((a, b) => a.CompareTo(b));
+        WriteList(ints);
+        Console.WriteLine("Order by down");
+        ints = integers.MyOrderBydown((a, b) => a.CompareTo(b));
+        WriteList(ints);
+    }
+
+    public static void WriteList<T>(List<T> list)
+    {
+        foreach (T item in list)
+        {
+            Console.WriteLine(item);
         }
     }
 }
